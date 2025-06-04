7ff8f0c231147c9b8c2acdf78068958fd05ae281
Muhammad Farhan Abdillah <122254033+thatFarhan@users.noreply.github.com>
2025-06-04 19:10:30 +0700

    Added Chat and ChatChannel

---
+++ Chat.java
@@ -0,0 +1,27 @@
+package com.isiair.isiairmobile;
+
+public class Chat {
+	private String message;
+	private Profile sender;
+	private ChatChannel channel;
+	
+	public Chat(String message, Profile sender, ChatChannel channel) {
+		super();
+		this.message = message;
+		this.sender = sender;
+		this.channel = channel;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Profile getSender() {
+		return sender;
+	}
+	
+	public void printInfo() {
+		System.out.println("From: " + sender.getName());
+		System.out.println("Message:\n" + message);
+	}
+}
---
+++ ChatChannel.java
@@ -0,0 +1,20 @@
+package com.isiair.isiairmobile;
+
+public class ChatChannel {
+	private int id;
+	private Admin admin;
+	private User user;
+	
+	public ChatChannel(int id, Admin admin, User user) {
+		super();
+		this.id = id;
+		this.admin = admin;
+		this.user = user;
+	}
+	
+	public void printInfo() {
+		System.out.println("id: " + id);
+		System.out.println("Admin: " + admin.getName());
+		System.out.println("User: " + user.getName());
+	}
+}
