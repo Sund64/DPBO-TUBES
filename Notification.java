5ca8d3defe41c106b5e428e3d6e13ae437eafdd5
Sund64 <chris.cornelis.lukito@gmail.com>
2025-06-04 17:55:07 +0700

    Notification.java

--- Notification.java
+++ Notification.java
@@ -1,16 +1,33 @@
 package halreward.tubes;
+import java.util.Date;
 
 public class Notification {
-	private User recipient;
-    private String message;
-
-    public Notification(User recipient, String message) {
-        this.recipient = recipient;
-        this.message = message;
-    }
-
-    public void send() {
-        System.out.println("Notifikasi kepada " + recipient.getName() + ": " + message);
-    }
+	private int id;
+	private String recipient;
+	private String message;
+	private Date sentAt;
+	public Notification(int id, String recipient, String message, Date sentAt) {
+		super();
+		this.id = id;
+		this.recipient = recipient;
+		this.message = message;
+		this.sentAt = sentAt;
+	}
+	public void send() {
+		System.out.println("Pesan dikirim ke " + this.recipient + " : " + this.message);
+	}
+	public void printInfo() {
+		System.out.println("----Notification Info----");
+		System.out.println("ID: " + this.id);
+		System.out.println("Recipient: " + this.recipient);
+		System.out.println("Message: " + this.message);
+		if (this.sentAt != null) {
+		System.out.println("Sent At: " + this.sentAt);
+		} else {
+			System.out.println("Belum terkirim");
+		}
+		System.out.println("------------------------");
+	}
+	
 }
-
+	
