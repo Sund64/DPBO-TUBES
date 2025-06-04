878661d85bd76766a77a0e678f9cfe414cd0e4fd
Sund64 <chris.cornelis.lukito@gmail.com>
2025-06-04 17:56:27 +0700

    QRIS.java

--- QRIS.java
+++ QRIS.java
@@ -1,17 +1,17 @@
 package DPBO.TUBES;
 
 public class QRIS extends Pembayaran{
-	private int QrisID;
-
-	public QRIS(int price, String metode, int qrisID) {
-		super(price, metode);
-		QrisID = qrisID;
+	private int qrisID;
+	
+	public QRIS(int price, String metodePembayaran, String status, int id, int qrisID) {
+		super(price, "QRIS", status, id);
+		this.qrisID = qrisID;
 	}
 	public void tampilkanQr() {
-		System.out.println(QrisID);
+		System.out.println("Tampilankan QR untuk id: " + this.qrisID);
 	}
-	public void pengecekanPembayaran() {
-		return;
+	public void verifikasiPembayaran() {
+		System.out.println("Pembayaran QRIS untuk id: " + this.qrisID + " sudah terverifikasi");
 	}
 	
 }
