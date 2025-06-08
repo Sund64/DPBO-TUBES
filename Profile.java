a19a37fb4d39386013d23e2ff367bc86918589fa
Faliqaqil <faliq.aqil.ra@gmail.com>
2025-06-08 21:29:07 +0700

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,37 +1,48 @@
-public class Profile {
-	private int id;
-	private String name, email, password;
-	
-	public Profile(int id, String name, String email, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
+public class Profile implements Identity {
+    private int id;
+    private String name, email, password;
+    
+    public Profile(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public boolean checkPassword(String inputPassword) {
-		return inputPassword.equals(password);
-	}
-	
-	public void resetPassword(String newPassword) {
-		this.password = newPassword;
-		System.out.println("Password berhasil di ubah");
-	}
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkPassword(String inputPassword) {
+        return inputPassword.equals(password);
+    }
+
+    public void resetPassword(String newPassword) {
+        this.password = newPassword;
+        System.out.println("Password berhasil diubah");
+    }
 }
