c83c5a9cd5e984ebda6c764c7f1b5dfd8df36883
Faliqaqil <faliq.aqil.ra@gmail.com>
2025-06-08 21:50:00 +0700

    Update Register.java

--- Register.java
+++ Register.java
@@ -1,25 +1,19 @@
 import java.util.ArrayList;
 
-public class Register implements Identity {
-    private int id;
-    private String name;
-    private String email;
-    private String password;
+public class Register extends User implements Identity {
     private String address;
     
     private static ArrayList<String> registeredNames = new ArrayList<>(); // Menyimpan daftar nama yang sudah digunakan
 
-    public Register(String name, String email, String address, String password) throws Exception {
+    public Register(int id, String name, String email, String password, int phoneNumber, char gender, String birthday, String address) throws Exception {
+        super(id, name, email, password, phoneNumber, gender, birthday);
         if (!email.endsWith("@gmail.com")) {
             throw new Exception("Email harus menggunakan domain @gmail.com");
         }
         if (AlreadyUse(name)) {
             throw new Exception("Nama ini sudah digunakan.");
         }
-        this.name = name;
-        this.email = email;
         this.address = address;
-        this.password = password;
         registeredNames.add(name); // Menyimpan nama ke dalam daftar
         System.out.println("Registrasi berhasil.");
     }
@@ -28,37 +22,24 @@ public class Register implements Identity {
         return registeredNames.contains(name);
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void printInfo() {
         System.out.println("===== INFO REGISTRASI =====");
-        System.out.println("Nama: " + name);
-        System.out.println("Email: " + email);
+        System.out.println("Nama: " + getName());
+        System.out.println("Email: " + getEmail());
         System.out.println("Alamat: " + address);
+        System.out.println("Nomor Telepon: " + getPhoneNumber());
+        System.out.println("Jenis Kelamin: " + getGender());
+        System.out.println("Tanggal Lahir: " + getBirthday());
         System.out.println("===========================");
     }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-}
-        user.printInfo();
-        scanner.close();
-	}
 }
 
 import java.util.Scanner;
