257766162ab6faaf317fdf10764d2e26a8eabf20
Faliqaqil <faliq.aqil.ra@gmail.com>
2025-06-10 16:11:39 +0700

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,8 +1,9 @@
-public class Profile implements Identity {
+abstract class Profile implements Identity {
     private int id;
-    private String name, email;
-    protected String password;
-    
+    private String name;
+    private String email;
+    private String password;
+
     public Profile(int id, String name, String email, String password) {
         this.id = id;
         this.name = name;
@@ -20,30 +21,13 @@ public class Profile implements Identity {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String getPassword() {
         return password;
-    }
-
-    public boolean checkPassword(String inputPassword) {
-        return inputPassword.equals(password);
-    }
-
-    public void resetPassword(String newPassword) {
-        this.password = newPassword;
-        System.out.println("Password berhasil diubah");
     }
 }
